9a6930123a94ddcae08419b4af108710d955e510
YuriyBerketa <115158490+YuriyBerketa@users.noreply.github.com>
2023-03-12 21:23:08 +0200

    add 01 hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const pictureMarkup = createGalleryItems(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', pictureMarkup);
+
+function createGalleryItems(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+  }).join('');
+}
+
+const galleryOptions = {
+    captionDelay: 250,
+    captionsData: 'alt',
+}
+const lightbox = new SimpleLightbox('.gallery a', galleryOptions);
