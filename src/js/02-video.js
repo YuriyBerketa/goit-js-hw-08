af8579ecef6bd0163f6b83aa0b546a7ae51950ac
YuriyBerketa <115158490+YuriyBerketa@users.noreply.github.com>
2023-03-13 12:04:47 +0200

    add 02 hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(function (time) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(time));
+}, 1000));
+
+const saveTime = localStorage.getItem('videoplayer-current-time');
+const timeStop = JSON.parse(saveTime);
+
+player.setCurrentTime(timeStop.seconds||0)
