cb56c49dea4cf78e6a88460f931bf510e4c2acf2
YuriyBerketa <115158490+YuriyBerketa@users.noreply.github.com>
2023-03-13 18:40:30 +0200

    add 03 hw (почти готово)

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+// import throttle from 'lodash.throttle';
 
+
+const formEl = document.querySelector(".feedback-form");
+
+let inputRes = {};
+
+initForm();
+
+formEl.addEventListener('submit', evt => {
+    evt.preventDefault();
+    const formData = new FormData(formEl);
+    formData.forEach((value, name) => console.log(value, name));
+   
+    localStorage.removeItem('feedback-form-state');
+    //  inputRes = {};
+    evt.currentTarget.reset();
+})
+
+formEl.addEventListener('input', evt => {
+    inputRes[evt.target.name] = evt.target.value;
+    console.log(inputRes);
+    localStorage.setItem('feedback-form-state', JSON.stringify(inputRes));
+});
+
+function initForm() {
+    let infoByUser = localStorage.getItem('feedback-form-state');
+    // console.log(infoByUser);
+    if (infoByUser) {
+        infoByUser = JSON.parse(infoByUser);
+        console.log(infoByUser);
+        Object.entries(infoByUser).forEach(([name, value]) => {
+            // console.log(name, value);
+            inputRes[name] = value;
+            formEl.elements[name].value = value;
+        })
+    }
+}
